c486c2fe3a35ebd3cba6630397512d4278c52d7a
djy <1983877110@qq.com>
2025-08-27 20:23:40 +0800

    将模拟器启用设置从 true 更改为 false；更新 entrypoint.sh 中的 verify 设置为 false，以禁用验证功能; 更新cert-client中lib的模拟quote和eventlog生成

--- kms/dstack-app/entrypoint.sh
+++ kms/dstack-app/entrypoint.sh
@@ -6,7 +6,7 @@ cat <<EOF > ./kms.toml
 admin_token_hash = "${ADMIN_TOKEN_HASH}"
 
 [core.image]
-verify = true
+verify = false
 cache_dir = "./images"
 download_url = "${IMAGE_DOWNLOAD_URL}"
 download_timeout = "2m"
